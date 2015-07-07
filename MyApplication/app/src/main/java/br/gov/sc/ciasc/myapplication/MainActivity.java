c422c4e1faeefa3d89bd1880826162d3c13d4b05
Carlos Alberto Nakazawa <carlos@ayoba.com.br>
2015-07-07 17:02:09 -0300

    Ciclo de vida de uma Activity

--- MyApplication/app/src/main/java/br/gov/sc/ciasc/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/br/gov/sc/ciasc/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package br.gov.sc.ciasc.myapplication;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,4 +18,39 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "Teste do Toast", Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "Entrou onStart...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "Entrou onResume...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "Entrou onPause...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "Entrou onStop...");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity", "Entrou onRestart...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "Entrou onDestroy...");
+    }
 }
