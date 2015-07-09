0e90a447d3626261dbb520593c50bbb10ec59cf1
Carlos Alberto Nakazawa <carlos@ayoba.com.br>
2015-07-09 19:22:15 -0300

    Atualização do DTO

--- Churrasco/app/src/main/java/br/gov/sc/ciasc/churrasco/dto/ChurrascoDto.java
+++ Churrasco/app/src/main/java/br/gov/sc/ciasc/churrasco/dto/ChurrascoDto.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 public class ChurrascoDto implements Serializable {
 
     private int carne;
+    private int linguica;
+    private int refrigerante;
 
     public int getCarne() {
         return carne;
@@ -12,5 +14,21 @@ public class ChurrascoDto implements Serializable {
 
     public void setCarne(int carne) {
         this.carne = carne;
+    }
+
+    public int getLinguica() {
+        return linguica;
+    }
+
+    public void setLinguica(int linguica) {
+        this.linguica = linguica;
+    }
+
+    public int getRefrigerante() {
+        return refrigerante;
+    }
+
+    public void setRefrigerante(int refrigerante) {
+        this.refrigerante = refrigerante;
     }
 }
