59cc6d667876fc8e37096e1f86813e29fd531781
Carlos Nakazawa <carlos@ayoba.com.br>
2015-08-03 19:52:54 -0300

    Primeira versão do teste

--- Soma/app/src/androidTest/java/br/gov/sc/ciasc/soma/ApplicationTest.java
+++ Soma/app/src/androidTest/java/br/gov/sc/ciasc/soma/ApplicationTest.java
@@ -1,13 +1,41 @@
 package br.gov.sc.ciasc.soma;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void test_um_mais_um_deve_ser_dois() throws Exception {
+        solo.assertCurrentActivity("Estou na Activity esperada", MainActivity.class);
+
+        assertTrue(solo.waitForText("Soma"));
+
+        solo.enterText(((EditText) solo.getView(R.id.primeiro)), "1");
+        solo.enterText(((EditText) solo.getView(R.id.segundo)), "1");
+
+        solo.clickOnButton("Somar");
+
+        assertTrue("Nao encontrou o resultado 2.", solo.waitForText("2", 1, 3));
+
+        solo.goBack();
+    }
+
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
     }
 }
--- Soma/app/src/main/java/br/gov/sc/ciasc/soma/MainActivity.java
+++ Soma/app/src/main/java/br/gov/sc/ciasc/soma/MainActivity.java
@@ -4,14 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText valorA;
+    EditText valorB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        valorA = (EditText) findViewById(R.id.primeiro);
+        valorB = (EditText) findViewById(R.id.segundo);
+    }
+
+    public void somar(View view) {
+        int resultado = 2;
+        Toast.makeText(this, "" + resultado, Toast.LENGTH_SHORT).show();
     }
 
     @Override
